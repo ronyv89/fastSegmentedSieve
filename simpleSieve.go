d8e4ff99c4b154af830177e820eefc02d28fc72b
Rony Varghese <ronyv250289@gmail.com>
2019-11-11 13:07:06 +0530

    Test

--- simpleSieve.go
+++ simpleSieve.go
@@ -1,27 +1,44 @@
 package fastsegmentedsieve
 
+import "math"
+
 // SimpleSieve returns prime numbers starting from 2 till the limit specified.
 func SimpleSieve(limit int64) []int64 {
-	primes := []int64{2}
-	var size int
-	if limit%2 == 0 {
-		size = int((limit - 1) / 2)
-	} else {
-		size = int(limit / 2)
+	primes := []int64{}
+	if limit >= 2 {
+		primes = append(primes, 2)
 	}
-
-	var unmark = make([]bool, size)
-	for p := int64(3); p*p < limit; p += 2 {
-		pos := int((p - 3) / 2)
-		if !unmark[pos] {
-			for i := pos + int(p); i < int(size); i += int(p) {
-				unmark[i] = true
+	if limit >= 3 {
+		primes = append(primes, 3)
+	}
+	if limit >= 5 {
+		multiple := int64(limit / 6)
+		if multiple*6+1 < limit {
+			multiple++
+		}
+		size := multiple * 2
+		unmark := make([]bool, size)
+		var startPos int64
+		for p := int64(1); 10*multiple > 6*p+1; p++ {
+			num1 := 6*p - 1
+			num2 := 6*p + 1
+		Num1Loop:
+			startPos = int64(math.Pow(10, float64(p-1))) + 7
+			for i := 0; startPos+num1*2*i < size; i++ {
+				unmark[startPos+num1*i] = true
+			}
+			startPos = p * 10
+			pos := int((p - 3) / 2)
+			if !unmark[pos] {
+				for i := pos + int(p); i < int(size); i += int(p) {
+					unmark[i] = true
+				}
 			}
 		}
-	}
-	for p := 0; p < size; p++ {
-		if !unmark[p] {
-			primes = append(primes, int64(p*2+3))
+		for p := 0; p < size; p++ {
+			if !unmark[p] {
+				primes = append(primes, int64(p*2+3))
+			}
 		}
 	}
 	return primes
